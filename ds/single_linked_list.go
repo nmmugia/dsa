201a26402be97894dcd9e590bad7bde065313400
nmmugia <seemugia1@gmail.com>
2023-05-04 08:11:24 +0700

    feat: finishing singleLinkedList
    Signed-off-by: nmmugia <seemugia1@gmail.com>

--- ds/single_linked_list.go
+++ ds/single_linked_list.go
@@ -1,5 +1,9 @@
 package ds
 
+import (
+	"fmt"
+)
+
 type Node struct {
 	value interface{}
 	next  *Node
@@ -35,6 +39,9 @@ func (sl *SingleLinkedList) Add(value interface{}) error {
 	if sl.head == nil {
 		sl.head = currentNode
 	}
+	if sl.tail != nil {
+		sl.tail.next = currentNode
+	}
 	sl.tail = currentNode
 	sl.length++
 	return nil
@@ -51,15 +58,72 @@ func (sl *SingleLinkedList) Remove(index int) error {
 		return nil
 	}
 	currentNode := sl.head
-	for i := 0; i < sl.Size(); i++ {
-		if i+1 == index {
-			if currentNode.next != nil {
-				currentNode.next = currentNode.next.next
+	for i := 0; i < sl.Size()-1; i++ {
+		if i+1 == index && currentNode.next != nil {
+			currentNode.next = currentNode.next.next
+			sl.length--
+			return nil
+		}
+		currentNode = currentNode.next
+	}
+	return fmt.Errorf("error failed removing node, node isn't founded, index:%d", index)
+}
+
+func (sl *SingleLinkedList) Search(index int) interface{} {
+	if index == 0 {
+		return sl.head.value
+	}
+	currentNode := sl.head
+	for i := 0; i < sl.Size()-1; i++ {
+		if i+1 == index && currentNode.next != nil {
+			return currentNode.next.value
+		}
+		currentNode = currentNode.next
+	}
+	return fmt.Errorf("node is not found, index: %d", index)
+}
+
+func (sl *SingleLinkedList) Insert(index int, value interface{}) error {
+	if index == sl.Size() || (index == 0 && sl.Size() == 0) {
+		return sl.Add(value)
+	}
+	currentNode := sl.head
+	for i := 0; i < sl.Size()-1; i++ {
+		if i+1 == index && currentNode.next != nil {
+			insertedNode := &Node{
+				value: value,
+				next:  currentNode.next,
 			}
+			currentNode.next = insertedNode
+			sl.length++
+			return nil
+		}
+		currentNode = currentNode.next
+	}
+	return fmt.Errorf("error failed inserting node, index unbound, index:%d", index)
+}
+
+func (sl *SingleLinkedList) Update(index int, value interface{}) error {
+	if index < 0 || index >= sl.Size() {
+		return fmt.Errorf("error failed update node, index unbound, index:%d", index)
+	}
+	if index == 0 {
+		sl.head.value = value
+	}
+	currentNode := sl.head
+	for i := 0; i < sl.Size()-1; i++ {
+		if i+1 == index && currentNode.next != nil {
+			currentNode.next.value = value
 			break
 		}
 		currentNode = currentNode.next
 	}
-	sl.length--
+	return nil
+}
+
+func (sl *SingleLinkedList) Clear() error {
+	sl.head = nil
+	sl.tail = nil
+	sl.length = 0
 	return nil
 }
--- ds/single_linked_list_test.go
+++ ds/single_linked_list_test.go
@@ -42,8 +42,9 @@ func TestCreationSingleLinkedList(t *testing.T) {
 }
 
 func TestManipulationSingleLinkedList(t *testing.T) {
-	t.Run(`Test adding elements to the list: Verify that elements can be added to the list, one at a time or in bulk,
-		and that the size of the list reflects the number of elements added.`,
+	t.Run(`Test adding elements to the list: Verify that elements can be added
+		to the list, one at a time or in bulk, and that the size of the list
+		reflects the number of elements added.`,
 		func(t *testing.T) {
 			sl := ds.NewSingleLinkedList()
 			assert.Nil(t, sl.Add("test"))
@@ -52,16 +53,92 @@ func TestManipulationSingleLinkedList(t *testing.T) {
 			assert.Nil(t, sl.Add("2"))
 			assert.Equal(t, 2, sl.Size())
 		})
-	t.Run(`Test removing elements from the list: Confirm that elements can be removed from the list, one at a time or in bulk,
-		and that the size of the list reflects the number of elements removed.`,
+	t.Run(`Test removing elements from the list: Confirm that elements can be
+		removed from the list, one at a time or in bulk, and that the size of
+		the list reflects the number of elements removed.`,
 		func(t *testing.T) {
 			sl := ds.NewSingleLinkedList()
 			assert.Nil(t, sl.Add("test"))
-			assert.False(t, sl.IsEmpty())
 			assert.Equal(t, 1, sl.Size())
 			assert.Nil(t, sl.Add("2"))
 			assert.Equal(t, 2, sl.Size())
 			assert.Nil(t, sl.Remove(0))
 			assert.Equal(t, 1, sl.Size())
+		})
+	t.Run(`Test retrieving elements from the list: Test that elements can be retrieved
+		from the list by index or by value, and that the correct element is returned.`,
+		func(t *testing.T) {
+			sl := ds.NewSingleLinkedList()
+			assert.Nil(t, sl.Add("test"))
+			assert.Nil(t, sl.Add("1"))
+			assert.Equal(t, 2, sl.Size())
+			assert.Equal(t, "test", sl.Search(0))
+			assert.Equal(t, "1", sl.Search(1))
+			assert.Nil(t, sl.Add("2"))
+			assert.Nil(t, sl.Add("3"))
+			assert.Equal(t, 4, sl.Size())
+			assert.Equal(t, "3", sl.Search(3))
+		})
+	t.Run(`Test inserting elements at a specific index: Test that elements can be
+		inserted at a specific index in the list, and that the size of the list
+		reflects the addition of the new element.`,
+		func(t *testing.T) {
+			sl := ds.NewSingleLinkedList()
+			assert.Nil(t, sl.Add("0"))
+			assert.Nil(t, sl.Add("1"))
+			assert.Equal(t, "1", sl.Search(1))
+			assert.Equal(t, 2, sl.Size())
+			assert.Nil(t, sl.Insert(1, "pushed"))
+			assert.Equal(t, 3, sl.Size())
+			assert.Equal(t, "pushed", sl.Search(1))
+			assert.Equal(t, "0", sl.Search(0))
+			assert.Equal(t, "1", sl.Search(2))
+		})
+	t.Run(`Test modifying elements in the list: Verify that elements in the
+		list can be modified, and that the modified element is correctly reflected in the list.`,
+		func(t *testing.T) {
+			sl := ds.NewSingleLinkedList()
+			assert.Nil(t, sl.Add("test"))
+			assert.Nil(t, sl.Add("1"))
+			assert.Nil(t, sl.Insert(1, "pushed"))
+			// values should be [test, pushed, 1]
+			assert.Equal(t, 3, sl.Size())
+			assert.Equal(t, "pushed", sl.Search(1))
+			assert.Nil(t, sl.Update(1, "updated1"))
+			assert.Equal(t, "updated1", sl.Search(1))
+			assert.Nil(t, sl.Update(0, "updated0"))
+			assert.Equal(t, "updated0", sl.Search(0))
+			assert.Nil(t, sl.Update(2, "updated2"))
+			assert.Equal(t, "updated2", sl.Search(2))
+		})
+	t.Run(`Test clearing the list: Confirm that the list can be cleared of all elements,
+		and that the size of the list is zero after clearing.`,
+		func(t *testing.T) {
+			sl := ds.NewSingleLinkedList()
+			assert.Nil(t, sl.Add("test"))
+			assert.Nil(t, sl.Add("1"))
+			assert.Nil(t, sl.Insert(1, "pushed"))
+			// values should be [test, pushed, 1]
+			assert.Equal(t, 3, sl.Size())
+			assert.Nil(t, sl.Clear())
+			assert.Equal(t, 0, sl.Size())
+		})
+	t.Run(`Test handling edge cases: Test the behavior of the list when it is empty, has
+		only one element, or has multiple elements. Test the behavior of the list when attempting
+		to add, remove, or retrieve elements that are out of bounds or not present in the list.`,
+		func(t *testing.T) {
+			sl := ds.NewSingleLinkedList()
+			assert.Nil(t, sl.Add("test"))
+			assert.Nil(t, sl.Add("1"))
+			assert.Nil(t, sl.Insert(1, "pushed"))
+			// values should be [test, pushed, 1]
+
+			assert.Equal(t, 3, sl.Size())
+			assert.Error(t, sl.Insert(500, "pushed"))
+			assert.Equal(t, 3, sl.Size()) // stay the same since the above line failed inserting
+			assert.Error(t, sl.Remove(500))
+			assert.Equal(t, 3, sl.Size()) // stay the same since the above line failed removing
+			assert.Error(t, sl.Update(500, "update"))
+			assert.Error(t, sl.Search(500).(error))
 		})
 }
